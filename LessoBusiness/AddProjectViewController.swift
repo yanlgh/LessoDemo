6a9781d5ec07b2c9f10aecd7e45b03da7119c267
lovcong <408390184@qq.com>
2015-12-10 14:25:56 +0800

    add project module

--- LessoBusiness/AddProjectViewController.swift
+++ LessoBusiness/AddProjectViewController.swift
@@ -9,76 +9,11 @@
 import UIKit
 import Eureka
 
-class AddProjectViewController: FormViewController {
+class AddProjectViewController: UITableViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-            
-        form = Section()
-            <<< SegmentedRow<String>("segments"){
-                $0.options = ["基本信息", "四方信息", "供货情况"]
-                $0.value = "基本信息"
-            }
-            +++ Section(){
-                $0.tag = "基本信息"
-                $0.hidden = "$segments != '基本信息'" // .Predicate(NSPredicate(format: "$segments != 'Sport'"))
-            }
-            <<< TextRow(){
-                $0.title = "Which is your favourite soccer player?"
-            }
-            
-            <<< TextRow(){
-                $0.title = "Which is your favourite coach?"
-            }
-            
-            <<< TextRow(){
-                $0.title = "Which is your favourite team?"
-            }
-            
-            +++ Section(){
-                $0.tag = "基本信息"
-                $0.hidden = "$segments != '基本信息'" // .Predicate(NSPredicate(format: "$segments != 'Sport'"))
-            }
-            <<< TextRow(){
-                $0.title = "Which is your favourite soccer player?"
-            }
-            
-            <<< TextRow(){
-                $0.title = "Which is your favourite coach?"
-            }
-            
-            <<< TextRow(){
-                $0.title = "Which is your favourite team?"
-            }
-            
-            +++ Section(){
-                $0.tag = "四方信息"
-                $0.hidden = "$segments != '四方信息'"
-            }
-            <<< TextRow(){
-                $0.title = "Which music style do you like most?"
-            }
-            
-            <<< TextRow(){
-                $0.title = "Which is your favourite singer?"
-            }
-            <<< TextRow(){
-                $0.title = "How many CDs have you got?"
-            }
-            
-            +++ Section(){
-                $0.tag = "供货情况"
-                $0.hidden = "$segments != '供货情况'"
-            }
-            <<< TextRow(){
-                $0.title = "Which is your favourite actor?"
-            }
-            
-            <<< TextRow(){
-                $0.title = "Which is your favourite film?"
-        }
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -87,14 +22,8 @@ class AddProjectViewController: FormViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
